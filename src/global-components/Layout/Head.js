0f325d89ef8356ad586f0f48c7678e40b69478af
Roland Branten <roland.branten@gmail.com>
2020-03-02 13:31:05 +0100

    added noindex

--- src/global-components/Layout/Head.js
+++ src/global-components/Layout/Head.js
@@ -1,4 +1,5 @@
 // Components==============
+import { Location } from "@reach/router";
 import { graphql, useStaticQuery } from "gatsby";
 import { useIntl } from "gatsby-plugin-intl";
 import React from "react";
@@ -15,50 +16,53 @@ export default function Head({ title, description, keywords, path }) {
           title
         }
       }
-      sitePage {
-        path
-      }
     }
   `);
   const siteTitle = data.site.siteMetadata.title;
-  const url = data.sitePage.path;
 
-  const noIndex = () => {
-    if (url.includes("nl") !== true || url.includes("en") !== true) {
+  const noIndex = location => {
+    const url = location.pathname;
+
+    if (url.includes("nl") || url.includes("en")) {
       return ``;
+    } else {
+      return `noindex`;
     }
-    return `noindex`;
   };
 
   return (
-    <Helmet>
-      <meta name="robots" content={noIndex()} />
-      <meta name="theme-color" content="#FFFFFF" />
-      <meta name="author" content="Roland Branten"></meta>
-      <meta name="designer" content="Roland Branten"></meta>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-      <link
-        rel="canonical"
-        href={`https://www.rolandbranten.nl/${intl.locale}/${path}`}
-      />
-      <link
-        rel="alternate"
-        href={`https://www.rolandbranten.nl/nl/${path}`}
-        hreflang="nl"
-      />
-      <link
-        rel="alternate"
-        href={`https://www.rolandbranten.nl/en/${path}`}
-        hreflang="en"
-      />
-      <link
-        rel="alternate"
-        href={`https://www.rolandbranten.nl/${path}`}
-        hreflang="x-default"
-      />
-      <title>{`${siteTitle} | ${title}`}</title>
-      <html lang={intl.locale} />
-    </Helmet>
+    <Location>
+      {({ location }) => (
+        <Helmet>
+          <meta name="robots" content={noIndex(location)} />
+          <meta name="theme-color" content="#FFFFFF" />
+          <meta name="author" content="Roland Branten"></meta>
+          <meta name="designer" content="Roland Branten"></meta>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <link
+            rel="canonical"
+            href={`https://www.rolandbranten.nl/${intl.locale}/${path}`}
+          />
+          <link
+            rel="alternate"
+            href={`https://www.rolandbranten.nl/nl/${path}`}
+            hreflang="nl"
+          />
+          <link
+            rel="alternate"
+            href={`https://www.rolandbranten.nl/en/${path}`}
+            hreflang="en"
+          />
+          <link
+            rel="alternate"
+            href={`https://www.rolandbranten.nl/${path}`}
+            hreflang="x-default"
+          />
+          <title>{`${siteTitle} | ${title}`}</title>
+          <html lang={intl.locale} />
+        </Helmet>
+      )}
+    </Location>
   );
 }
